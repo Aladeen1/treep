1b953be688587bd50495bc2fa8c5795c309d3e39
tanguy Colou <tanguy.colou@skema.edu>
2019-04-11 15:34:02 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/search', to:'pages#search'
   get '/compensation', to:'pages#compensation'
   get '/dashboard', to:'pages#dashboard'
+  get '/about', to:'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
