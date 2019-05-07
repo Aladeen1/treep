9470d328a7fd8656705d673ff6214676efec2623
Valentin Raille <Valrail78@gmail.com>
2019-05-07 18:57:46 +0200

    payment connected

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/search', to:'pages#search'
-  get '/compensation', to:'pages#compensation'
   get '/dashboard', to:'pages#dashboard'
   get '/about', to:'pages#about'
   get '/lesarbres', to:'pages#lesarbres'
+  resources :compensations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
